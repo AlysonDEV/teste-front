b241c5664b9a670fa3a6d368d316f3a4e77128aa
AlysonDev <devalyson@gmail.com>
2023-03-28 18:27:59 -0300

    Correção de fastrefresh no vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import sass from 'vite-plugin-sass';
 
-import reactRefresh from '@vitejs/plugin-react-refresh';
 
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sass(), reactRefresh()],
+  plugins: [react(), sass()],
   optimizeDeps: {
     include: [
       'react-toastify',
