1f441513e7698c422e1f634e9620be09a51d3207
AlysonDev <devalyson@gmail.com>
2023-03-28 05:28:19 -0300

    Criando o view pacientes

---
+++ src/components/ViewPacientRecent/index.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import { Button, Table } from "react-bootstrap";
+import { RiArrowRightLine } from "react-icons/ri";
+import { Patient } from "../../vite-env";
+
+import { api } from "../../service/api";
+
+import { showNotification } from "../Notification";
+
+export function ViewPacientRecent() {
+  const [patients, setPatients] = useState<Patient[]>([]);
+
+  useEffect(() => {
+    api('pacientes')
+      .then(response => {
+        setPatients(response.data);
+      })
+      .catch(error => {
+        showNotification({
+          message: `Não foi possível carregar a lista de pacientes: ${error}`,
+          type: 'error'
+        })
+      });
+  }, []);
+
+  function calculateAge(dateOfBirth: Date): number {
+    const today = new Date();
+    const age = formatDistanceStrict(dateOfBirth, today, { unit: 'year' });
+    return Number(age);
+  }
+
+
+
+  return (
+    <>
+      <h3>Pacientes</h3>
+      <Table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Condição</th>
+            <th>CPF</th>
+            <th>Idade</th>
+            <th>...</th>
+          </tr>
+        </thead>
+        <tbody>
+          {patients.map(patient => (
+            <tr key={patient.id}>
+              <td>{patient.nome}</td>
+              <td></td>
+              <td>{patient.cpf}</td>
+              <td>{patient.dt_nascimento}</td>
+              <td>
+                <Button>
+                  <RiArrowRightLine />
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+
+      </Table>
+
+    </>
+  )
+
+}
+
+function formatDistanceStrict(dateOfBirth: Date, today: Date, arg2: { unit: string; }) {
+  throw new Error("Function not implemented.");
+}
--- src/pages/Pacient/index.tsx
+++ src/pages/Pacient/index.tsx
@@ -1,11 +1,16 @@
+import { ToastContainer } from "react-toastify";
 import { ModalPacientRegister } from "../../components/ModalPacientRegister";
+import { ViewPacientRecent } from "../../components/ViewPacientRecent";
 
 
 export function Pacient() {
   return (
     <>
+      <ToastContainer />
       <h2>Cadastro de pacientes</h2>
       <ModalPacientRegister />
+      <ViewPacientRecent />
+
     </>
   )
 }
