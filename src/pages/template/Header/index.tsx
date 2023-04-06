a84f907fc56d905d00b769493ef650b953a1e0bf
AlysonDev <devalyson@gmail.com>
2023-04-06 03:39:19 -0300

    notficações da aplicação

--- src/pages/template/Header/index.tsx
+++ src/pages/template/Header/index.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 export function Header() {
   return (
     <header>
+      <ToastContainer />
       <h1>Multintegrada</h1>
       <Link to="/">Home</Link>
       <Link to="/pacient">Paciente</Link>
